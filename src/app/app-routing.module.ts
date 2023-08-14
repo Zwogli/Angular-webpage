83610d986cecaa8dbbc8eaf5f5d9726e82247955
Zwogli <Mathiaskohler@web.de>
2023-08-14 10:03:56 +0200

    implement Imprint with if/else statement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+/**
+ * Implement links
+ */
+const routes: Routes = [
+  { path: '', component: SlideshowComponent }, // Need : import { [component] } from '[component path]';
+  { path: 'imprint', component: ImprintComponent } // Need: import { ImprintComponent } from './imprint/imprint.component';
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
