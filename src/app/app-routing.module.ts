580134e94bfb7125df926641fdb00d8461351ba5
Zwogli <Mathiaskohler@web.de>
2023-08-14 10:16:01 +0200

    implement data-protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 /**
  * Implement links
  */
 const routes: Routes = [
   { path: '', component: SlideshowComponent }, // Need : import { [component] } from '[component path]';
-  { path: 'imprint', component: ImprintComponent } // Need: import { ImprintComponent } from './imprint/imprint.component';
+  { path: 'imprint', component: ImprintComponent }, // Need: import { ImprintComponent } from './imprint/imprint.component';
+  { path: 'data-protection', component: DataProtectionComponent }
 ];
 
 @NgModule({
